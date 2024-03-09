4eb884b3ba826167b3ac9e9df625a951183110da
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-03-09 11:01:56 +0100

    feat(app): #13 set up landing page

---
+++ apps/donate/components/welcome/index.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+export default function Welcome_view() {
+    return (
+        <Typography>Welcome view for mobile landing page</Typography>
+    )
+}
