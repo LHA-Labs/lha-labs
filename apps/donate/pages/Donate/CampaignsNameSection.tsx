3bdcb3a03d7c3c71f32ee8fe127eb6230036f01c
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-03-15 09:21:17 +0100

    feat(app): #20 set campaign name section

---
+++ apps/donate/pages/Donate/CampaignsNameSection.tsx
@@ -0,0 +1,68 @@
+import { Box, Button, Typography } from '@mui/material';
+import Image from 'next/image';
+import React from 'react';
+import campaignsNameImg from '../../assets/campaignsNameImg.png';
+import { useIntl } from 'react-intl';
+
+export default function CampaignsNameSection() {
+  const { formatMessage } = useIntl();
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '0px 118px',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+          gap: '4px',
+        }}
+      >
+        <Typography variant="h4" sx={{ color: 'var(--primary)' }}>
+          {formatMessage({ id: 'campaignsName' })}
+        </Typography>
+        <Typography variant="h2" sx={{ color: 'var(--titleActive)' }}>
+          {formatMessage({id:"NameOfCampaign"})}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Image src={campaignsNameImg} alt="Campaigns Name Imgage"></Image>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '10px',
+            borderRadius: '200px',
+            background: 'rgba(206, 0, 0, 0.10)',
+            padding: '10px',
+          }}
+        >
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            {formatMessage({id:"campaignNameText"})}
+           L{' '}
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            sx={{ borderRadius: '50px' }}
+          >
+           {formatMessage({id:"viewObjective"})} 
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- apps/donate/pages/Donate/Donate.tsx
+++ apps/donate/pages/Donate/Donate.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import DonatePresentationSection from './DonatePresentationSection';
+import CampaignsNameSection from './CampaignsNameSection';
 
 export default function Donate() {
   return (
     <Box>
       <DonatePresentationSection />
+      <CampaignsNameSection />
     </Box>
   );
 }
