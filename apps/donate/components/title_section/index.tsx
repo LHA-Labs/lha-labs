d845ef83764962c7b97f75a1912afc28dfc04da9
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-03-18 12:33:45 +0100

    feat(app): create section title component

---
+++ apps/donate/components/title_section/index.tsx
@@ -0,0 +1,33 @@
+import { Box, Typography } from "@mui/material";
+
+function SectionTitle({ title, label }: { title: string, label: string }) {
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: '10px 0'
+        }}>
+            <Typography sx={{
+                color: 'var(--primary)',
+                fontWeight: 'var(--medium)',
+            }}>{title}</Typography>
+            <Typography sx={{
+                textAlign: 'center',
+                fontWeight: 'var(--semiBold)',
+                fontSize: '20px',
+                lineHeight: '24px',
+                color: 'var(--titleActive)'
+
+            }}>{`${label}`}</Typography>
+            <Box component='span' sx={{
+                border: '1px solid var(--primary)',
+                width: '90px',
+                marginTop: '10px'
+            }}></Box>
+        </Box>
+
+    );
+}
+
+export default SectionTitle;
