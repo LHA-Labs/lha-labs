84423fe9c5f54eac14f09ac851512927e1458fef
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-23 18:52:31 +0100

    docs(app): #2 added french translations for layout.
    Closes: #2

--- libs/theme/src/languages/fr/website.ts
+++ libs/theme/src/languages/fr/website.ts
@@ -2,21 +2,15 @@ export const website = {
   home: 'Accueil',
   aboutUs: 'À propos de nous',
   partners: 'Partenaires',
-  donate: 'Donner',
+  donate: 'Faire un don',
   makeADonation: 'Faire un don',
   en: 'En',
   fr: 'Fr',
-
-  ['Nous joindre']: 'Join Us',
-
-  //Footer
-  ['Get involved']: "S'impliquer",
-  ['Get in touch']: 'Prendre contact',
-  Organisation: 'Organisation',
-  ['Lets Help Association, devoted to compassion and social impact, is committed to providing meaningful assistance to communities.']:
-    "L'association Lets Help, qui se consacre à la compassion et à l'impact social, s'engage à fournir une assistance significative aux communautés.",
-  ['About Us']: 'À propos de nous',
-  ['Partners']: 'Partenaires',
-  ['Make an anonymous donation']: 'Faire un don anonyme',
-  ['let’shelp@gmail.com']: "let'shelp@gmail.com",
+  joinUs: 'Rejoignez-nous',
+  getInvolved: 'Impliquez-vous',
+  getInvolvedMessage:
+    "L'association Lets Help, dédiée à la compassion et à l'impact social, s'engage à fournir une aide significative aux communautés.",
+  makeAnonymousDonation: 'Faire un don anonyme',
+  getInTouch: 'Nous contacter',
+  organisation: 'Organisation',
 };
