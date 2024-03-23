44337309fb62bf080155973e9d0beb23036dfe9d
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-23 21:48:40 +0100

    chore(app): #6 removed comments

--- apps/donate/pages/index.tsx
+++ apps/donate/pages/index.tsx
@@ -2,22 +2,8 @@ import { Box } from '@mui/material';
 import HeroSection from '../components/HeroSection/HeroSection';
 
 export function Index() {
-  // const { formatMessage } = useIntl();
-  // const { activeLanguage, languageDispatch } = useLanguage();
   return (
-    <Box sx={{ padding: 1.5 }}>
-      {/* <Button
-        variant="contained"
-        color="primary"
-        onClick={() =>
-          languageDispatch({
-            type: activeLanguage === 'en' ? 'USE_FRENCH' : 'USE_ENGLISH',
-          })
-        }
-      >
-        Change Language: {activeLanguage}
-      </Button>
-      <Typography variant="h1"> {formatMessage({ id: 'Hello' })}</Typography> */}
+    <Box sx={{ padding: 4 }}>
       <HeroSection />
     </Box>
   );
