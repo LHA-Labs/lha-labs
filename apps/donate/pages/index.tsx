26bb94e2a7878a04f25fff89fac5ac3b3aafd787
Pierre Poukam Ngamaleu <ngamaleu2011@gmail.com>
2024-03-24 00:40:52 +0100

    Donate landing about us (#18)
    * build(shared): #1 installed sass
    
    * refactor(app): #1 corrected styles colors
    
    * refactor(app): #1 imported styles and moved layout to right spot
    
    * docs(shared): #5 adjusted and set a basis for the repo readme file
    
    * build(app): install better-commits library
    
    * feat(app): #13 set up landing page
    
    * feat(app): #13 set up image grid from welcome view
    
    * feat(app): #13 set up welcome view componant and padding
    
    * refactor(app): #13 add image illustration of childs
    
    * feat(app): #13 add support message
    
    * feat(app): #13 set up button action
    
    * feat(app): #17 set up initail required for about us comp
    
    * feat(app): #17 set up all about us component
    
    * fix(app): #13 remove padding from parent landing view to each component
    
    * refactor(app): #33 renamed aboutUs to suit naming convention
    
    * refactor(app): #33 extracted interface and renamed var
    
    * refactor(app): #33 adjusted ds labels, updated ds description and adjusted presentation
    
    * refactor(app): #33 use utility to present numbers
    
    * refactor(app): #33 adjusted button radius, elevation, text translation and layout
    
    * refactor(app): #33 adjusted about us messages
    
    * refactor(app): #33 removed about_us css module
    
    * refactor(app): #33 adjusted css, changed flex for grid
    
    * refactor(app): #33 adjusted layout and some css
    
    * refactor(app): #6 adjuste hero subtitle mobile fontSize
    
    ---------
    
    Co-authored-by: Tchakoumi <lorraintchakoumi@gmail.com>
    Co-authored-by: Tchakoumi Lorrain <43315117+Tchakoumi@users.noreply.github.com>

--- apps/donate/pages/index.tsx
+++ apps/donate/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Box } from '@mui/material';
 import HeroSection from '../components/HeroSection/HeroSection';
+import AboutUs from '../components/AboutUs/AboutUs';
 
 export function Index() {
   return (
     <Box sx={{ padding: '32px 0' }}>
       <HeroSection />
+      <AboutUs />
     </Box>
   );
 }
