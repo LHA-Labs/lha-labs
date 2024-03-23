a524557f62a8dec34def19e8efe6b126b5a7913e
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-23 22:25:17 +0100

    feat(app): #6 adjusted herosection for responsivity

--- apps/donate/components/HeroSection/HeroSection.tsx
+++ apps/donate/components/HeroSection/HeroSection.tsx
@@ -5,55 +5,57 @@ import HeroImageGrid from './HeroImageGrid/HeroImageGrid';
 export default function HeroSection() {
   const { formatMessage } = useIntl();
   return (
-    <Box component="section">
-      <Typography
-        variant="h2"
-        sx={{
-          lineHeight: '36px',
-          fontSize: '30px',
-          letterSpacing: '-0.02em',
-          textAlign: 'left',
-          color: '#333',
-        }}
-      >
-        {formatMessage({ id: 'heroSectionTitle' })}
-      </Typography>
-      <Typography
-        variant="h5"
-        sx={{
-          lineHeight: '20px',
-          fontSize: '12px',
-          fontWeight: '600',
-          letterSpacing: '0em',
-          textAlign: 'left',
-          color: '#666',
-        }}
-      >
-        {formatMessage({ id: 'heroSectionSubtitle' })}
-      </Typography>
-      <Box
-        sx={{
-          display: 'grid',
-          gridAutoFlow: 'column',
-          alignItems: 'center',
-          columnGap: 2,
-        }}
-      >
-        <Button
-          variant="contained"
-          sx={{ borderRadius: '20px' }}
-          disableElevation
+    <Box
+      component="section"
+      sx={{ display: 'grid', gridTemplateColumns: {laptop:'1fr 1.4fr', mobile:'none'}, padding:{laptop:'0 118px', mobile:'0 32px'} }}
+    >
+      <Box>
+        <Typography
+          variant="h2"
+          sx={{
+            lineHeight: {mobile:'36px',laptop:'70.5px'},
+            fontSize: {mobile:'30px', laptop:'56px'},
+            letterSpacing: '-0.02em',
+            color: '#333',
+          }}
         >
-          {formatMessage({ id: 'donate' })}
-        </Button>
-        <Button
-          variant="contained"
-          sx={{ borderRadius: '20px' }}
-          disableElevation
-          color="secondary"
+          {formatMessage({ id: 'heroSectionTitle' })}
+        </Typography>
+        <Typography
+          variant="h5"
+          sx={{
+            lineHeight: {mobile:'20px',laptop:'28px'},
+            fontSize: {mobile:'12px', laptop:'18px'},
+            fontWeight: '600',
+            color: '#666',
+          }}
         >
-          {formatMessage({ id: 'joinUs' })}
-        </Button>
+          {formatMessage({ id: 'heroSectionSubtitle' })}
+        </Typography>
+        <Box
+          sx={{
+            display: 'grid',
+            gridAutoFlow: 'column',
+            alignItems: 'center',
+            columnGap: 2,
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{ borderRadius: '20px' }}
+            disableElevation
+          >
+            {formatMessage({ id: 'donate' })}
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ borderRadius: '20px' }}
+            disableElevation
+            color="secondary"
+          >
+            {formatMessage({ id: 'joinUs' })}
+          </Button>
+        </Box>
       </Box>
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <HeroImageGrid />
