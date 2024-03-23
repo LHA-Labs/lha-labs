b1362962b2bde55967eaa4927d043a35bdcd660d
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-23 17:48:36 +0100

    chore(app): #2 removed comment from layout

--- apps/donate/components/layout/Index.tsx
+++ apps/donate/components/layout/Index.tsx
@@ -1,12 +1,9 @@
 import { Box } from '@mui/material';
+import { PropsWithChildren } from 'react';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 
-export default function DonateLayout({ children }: { children: JSX.Element }) {
-  /* TODO: this function will receive
-    the header, footer and makeof it the layout,
-    such that other components just load into it
-  */
+export default function DonateLayout({ children }: PropsWithChildren) {
   return (
     <Box
       sx={{
