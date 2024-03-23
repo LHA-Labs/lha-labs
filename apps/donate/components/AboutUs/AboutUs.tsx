465cd19139333cbff6ad0612cd43419ee5960af1
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-24 00:34:36 +0100

    refactor(app): #33 adjusted layout and some css

--- apps/donate/components/AboutUs/AboutUs.tsx
+++ apps/donate/components/AboutUs/AboutUs.tsx
@@ -20,7 +20,10 @@ function AboutUs() {
   return (
     <Box
       component="section"
-      sx={{ backgroundColor: theme.common.inputBackground, padding: 1.5 }}
+      sx={{
+        backgroundColor: theme.common.inputBackground,
+        padding: { mobile: '12px 32px', laptop: '48px 118px' },
+      }}
     >
       <Box
         sx={{
@@ -51,70 +54,91 @@ function AboutUs() {
           }}
         />
       </Box>
-      <Typography
-        sx={{
-          fontSize: '14px',
-          fontWeight: 'var(--semiBold)',
-          padding: '20px 0',
-          lineHeight: '24px',
-        }}
-      >
-        {formatMessage({ id: 'aboutUsMessage' })}
-      </Typography>
       <Box
         sx={{
           display: 'grid',
-          gridAutoFlow: 'column',
-          alignItems: 'center',
-          columnGap: 2,
+          gridTemplateColumns: { laptop: '4.3fr 5.7fr', mobile: 'none' },
+          columnGap: 5,
+          alignItems: 'stretch',
         }}
       >
-        <Button
-          variant="contained"
-          disableElevation
-          sx={{ borderRadius: '20px' }}
-        >
-          {formatMessage({ id: 'joinUs' })}
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          disableElevation
-          sx={{ borderRadius: '20px' }}
-        >
-          {formatMessage({ id: 'moreAboutUs' })}
-        </Button>
-      </Box>
-      <Box
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'auto auto auto',
-          backgroundColor: '#fff',
-          gap: '20px',
-          margin: '20px 0',
-          padding: '10px',
-        }}
-      >
-        {letsHelpStatistics.map(({ label, value, unit }, index) => (
-          <Box key={index}>
-            <Typography
-              sx={{ fontSize: '30px', lineHeight: '36px', fontWeight: 'bold' }}
+        <Box>
+          <Typography
+            sx={{
+              fontSize: '14px',
+              fontWeight: 'var(--semiBold)',
+              padding: '20px 0',
+              lineHeight: '24px',
+              textAlign: 'justify',
+            }}
+          >
+            {formatMessage({ id: 'aboutUsMessage' })}
+          </Typography>
+          <Box
+            sx={{
+              display: 'grid',
+              gridAutoFlow: 'column',
+              alignItems: 'center',
+              columnGap: 2,
+            }}
+          >
+            <Button
+              variant="contained"
+              disableElevation
+              sx={{ borderRadius: '20px' }}
             >
-              +{`${shortenNumber(value)} ${unit ?? ''}`}
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: '13px',
-                fontWeight: 'var(--semiBold)',
-                lineHeight: '12px',
-                padding: '3px',
-                backgroundColor: 'rgba(165, 0, 0, 0.2)',
-              }}
+              {formatMessage({ id: 'joinUs' })}
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              disableElevation
+              sx={{ borderRadius: '20px' }}
             >
-              {formatMessage({ id: label })}
-            </Typography>
+              {formatMessage({ id: 'moreAboutUs' })}
+            </Button>
           </Box>
-        ))}
+        </Box>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            backgroundColor: '#fff',
+            gap: '20px',
+            margin: '20px 0',
+            padding: { mobile: '10px', tablet: '20px 73px' },
+            borderRadius: '10px',
+          }}
+        >
+          {letsHelpStatistics.map(({ label, value, unit }, index) => (
+            <Box key={index}>
+              <Typography
+                sx={{
+                  fontSize: '30px',
+                  lineHeight: '36px',
+                  fontWeight: 'bold',
+                  color: '#333',
+                }}
+              >
+                +{`${shortenNumber(value)} ${unit ?? ''}`}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: '13px',
+                  fontWeight: 'var(--semiBold)',
+                  lineHeight: '12px',
+                  padding: '3px',
+                  backgroundColor: 'rgba(165, 0, 0, 0.2)',
+                  width: 'fit-content',
+                  color: '#333',
+                  borderRadius: 0.5,
+                }}
+              >
+                {formatMessage({ id: label })}
+              </Typography>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
