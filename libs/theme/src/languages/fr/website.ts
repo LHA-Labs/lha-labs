3e693200d0658ac47d7816685f3ee309b673b186
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-03-23 14:41:40 +0100

    docs(app): #2 added french translation of words for header

--- libs/theme/src/languages/fr/website.ts
+++ libs/theme/src/languages/fr/website.ts
@@ -1,22 +1,22 @@
 export const website = {
-  Hello: 'Salut',
+  home: 'Accueil',
+  aboutUs: 'À propos de nous',
+  partners: 'Partenaires',
+  donate: 'Donner',
+  makeADonation: 'Faire un don',
+  en: 'En',
+  fr: 'Fr',
 
-  //Header
-  Home: "Accueil",
-  Donate: "Donation",
-  Eng: "Fr",
-
-  //Buttons
-  ["Make a donation"]:"Faire un don",
-  ["Nous joindre"]: "Join Us",
+  ['Nous joindre']: 'Join Us',
 
   //Footer
-  ["Get involved"]: "S'impliquer",
-  ['Get in touch']: "Prendre contact",
-  Organisation: "Organisation",
-  ['Lets Help Association, devoted to compassion and social impact, is committed to providing meaningful assistance to communities.']: "L'association Lets Help, qui se consacre à la compassion et à l'impact social, s'engage à fournir une assistance significative aux communautés.",
-  ["About Us"]: "À propos de nous",
-  ['Partners']: "Partenaires",
+  ['Get involved']: "S'impliquer",
+  ['Get in touch']: 'Prendre contact',
+  Organisation: 'Organisation',
+  ['Lets Help Association, devoted to compassion and social impact, is committed to providing meaningful assistance to communities.']:
+    "L'association Lets Help, qui se consacre à la compassion et à l'impact social, s'engage à fournir une assistance significative aux communautés.",
+  ['About Us']: 'À propos de nous',
+  ['Partners']: 'Partenaires',
   ['Make an anonymous donation']: 'Faire un don anonyme',
   ['let’shelp@gmail.com']: "let'shelp@gmail.com",
 };
