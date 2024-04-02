6a9ac23009eca638b10cfff8ae191d8144fed7d3
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-02 13:00:14 +0100

    refactor(app): #10 adjusted the image asset folder, and image positioning

--- apps/donate/components/about-us/LhaPresentationSection.tsx
+++ apps/donate/components/about-us/LhaPresentationSection.tsx
@@ -1,17 +1,9 @@
-import { shortenNumber } from '@lha-labs/utils';
-import { Divider, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import Image from 'next/image';
-import { useIntl } from 'react-intl';
-import imgOfTeam1 from '../../assets/imgOfTeam1.png';
-import imgOfTeam2 from '../../assets/imgOfTeam2.png';
-import { ILetsHelpStat, useStats } from '../../services/stats';
+import imgOfTeam1 from '../../public/assets/managing_team_1.png';
+import imgOfTeam2 from '../../public/assets/managing_team_2.png';
 
 export default function LhaPresentationSection() {
-  const { formatMessage } = useIntl();
-
-  const statistic: ILetsHelpStat[] = useStats();
-
   return (
     <Box
       sx={{
@@ -21,96 +13,44 @@ export default function LhaPresentationSection() {
     >
       <Box
         sx={{
-          display: 'flex',
-          gridTemplateRows: '1fr 2fr 1fr',
-          height: '100%',
-          width: '100%',
-          padding: '0px 118px',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          flexDirection: 'row',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          columnGap: 2,
+          padding: { mobile: '12px 32px', laptop: '48px 118px' },
         }}
       >
-        <Box sx={{ height: '100%', width: '100%' }}>
-          <Image src={imgOfTeam1} alt="Image 1" />
-        </Box>
+        <Image
+          src={imgOfTeam1}
+          alt="Managing team"
+          style={{ height: '100%', width: '100%', objectFit: 'cover' }}
+        />
 
         <Box
           sx={{
             display: 'grid',
             gridTemplateRows: '1fr 1fr',
-            justifyContent: 'space-between',
-            gap: '2px',
+            gap: 2,
             height: '100%',
             width: '100%',
           }}
         >
-          <Image src={imgOfTeam2} alt="Image 2" />
-          <Image src={imgOfTeam2} alt="Image 3" />
+          <Image
+            src={imgOfTeam2}
+            alt="Managing team"
+            style={{ height: '100%', width: '100%', objectFit: 'cover' }}
+          />
+          <Image
+            src={imgOfTeam2}
+            alt="Managing team"
+            style={{ height: '100%', width: '100%', objectFit: 'cover' }}
+          />
         </Box>
 
-        <Box sx={{ height: '100%', width: '100%' }}>
-          <Image src={imgOfTeam1} alt="Image 4" />
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: 'flex',
-          width: '100%',
-          padding: '0px 118px',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        {statistic.map(({ value, label, unit }, index) => (
-          <Box
-            key={index}
-            sx={{
-              display: 'flex',
-              width: '170px',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <Box
-              sx={{
-                display: 'flex',
-                width: 'auto',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'flex-start',
-              }}
-            >
-              <Typography variant="h1" sx={{ color: 'titleActive' }}>
-                {`+${shortenNumber(value)} ${unit ?? ''}`}
-              </Typography>
-              <Box
-                sx={{
-                  borderRadius: '4px',
-                  display: 'flex',
-                  padding: '0px 3px',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  gap: '10px',
-                  background: 'rgba(220, 0, 0, 0.20)',
-                }}
-              >
-                <Typography
-                  variant="body2"
-                  sx={{
-                    color: 'titleActive',
-                  }}
-                >
-                  {formatMessage({ id: `${label}` })}
-                </Typography>
-              </Box>
-            </Box>
-            {index !== statistic.length - 1 && (
-              <Divider orientation="vertical" variant="middle" flexItem />
-            )}
-          </Box>
-        ))}
+        <Image
+          src={imgOfTeam1}
+          alt="Managing team"
+          style={{ height: '100%', width: '100%', objectFit: 'cover' }}
+        />
       </Box>
     </Box>
   );
