f77dd15f9b41f5e81a5252285b53d14db1aac086
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-02 13:20:28 +0100

    chore(app): #10 adjusted stat order and removed unit from donations

--- apps/donate/services/stats.ts
+++ apps/donate/services/stats.ts
@@ -9,8 +9,8 @@ export function useStats() {
     { label: 'donors', value: 100 },
     { label: 'sponsors', value: 15 },
     { label: 'orphans', value: 500 },
-    { label: 'receivedDonations', value: 2000000, unit: 'XAF' },
     { label: 'partners', value: 16 },
+    { label: 'receivedDonations', value: 2000000 },
   ];
 
   return letsHelpStatistics;
