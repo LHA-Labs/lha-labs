f5a504ef67828d023a971c407c5697aed5a4eae4
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-02 13:27:11 +0100

    feat(app): #10 added lhaStats sectionto about us page

--- apps/donate/pages/about-us/index.tsx
+++ apps/donate/pages/about-us/index.tsx
@@ -1,14 +1,21 @@
 import { Box } from '@mui/material';
-import React from 'react';
 // import OurTeamSection from './OurTeamSection';
-import LhaPresentationSection from '../../components/about-us/LhaPresentationSection';
 import AboutUsHeader from '../../components/about-us/AboutUsHeader';
+import LhaPresentationSection from '../../components/about-us/LhaPresentationSection';
+import LhaStats from '../../components/about-us/LhaStats';
 
 export default function AboutUs() {
   return (
-    <Box sx={{ padding: '32px 0 0 0' }}>
+    <Box
+      sx={{
+        padding: '32px 0 0 0',
+        display: 'grid',
+        rowGap: { mobile: 1, laptop: 2 },
+      }}
+    >
       <AboutUsHeader />
       <LhaPresentationSection />
+      <LhaStats />
       {/* <OurTeamSection /> */}
     </Box>
   );
