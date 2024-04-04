81933a82c94d92a68f816b8af31d8c5efd01e4fb
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-04 17:50:11 +0100

    chore(app): #16 added bgImage to partners page...
    Closes: #16

--- apps/donate/pages/partners/index.tsx
+++ apps/donate/pages/partners/index.tsx
@@ -5,7 +5,14 @@ import OurSponsorsSection from '../../components/partners/OurSponsorsSection';
 
 export default function OurPartners() {
   return (
-    <Box sx={{ padding: {laptop:'0 0 80px  0', mobile:'0 0 24px  0'} }}>
+    <Box
+      sx={{
+        padding: { laptop: '0 0 80px  0', mobile: '0 0 24px  0' },
+        backgroundImage: 'url(/assets/campaigns_bg.png)',
+        backgroundPosition: '0 100px',
+        backgroundRepeat:'no-repeat'
+      }}
+    >
       <OurPartnersHeader />
       <OurSponsorsSection />
       <BecomeSponsorsSection />
