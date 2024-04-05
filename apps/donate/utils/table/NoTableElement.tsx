33e55241f01d3160cc25a3ff758f7e04d37f0912
Tchakoumi <lorraintchakoumi@gmail.com>
2024-04-05 13:27:17 +0100

    feat(app): #20 adjusted noTableElement props

--- apps/donate/utils/table/NoTableElement.tsx
+++ apps/donate/utils/table/NoTableElement.tsx
@@ -1,12 +1,16 @@
 import { TableCell, TableRow } from '@mui/material';
 import { useIntl } from 'react-intl';
 
-export function NoTableElement({ cols = 5 }: { cols?: number }) {
+interface NoTableElementProps {
+  cols?: number;
+  message?: string;
+}
+export function NoTableElement({ cols = 5, message }: NoTableElementProps) {
   const { formatMessage } = useIntl();
   return (
     <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
       <TableCell colSpan={cols} align="center">
-        {formatMessage({ id: 'nothingToDisplay' })}
+        {message ?? formatMessage({ id: 'nothingToDisplay' })}
       </TableCell>
     </TableRow>
   );
