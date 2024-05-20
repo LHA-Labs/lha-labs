ded0c05102e5ebbd9df7480b148380a18acaabc0
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-05-20 05:11:24 +0100

    feat: added nosifer font to project

--- apps/donate/pages/_document.tsx
+++ apps/donate/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Montserrat:wght@100..900&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Montserrat:wght@100..900&family=Nosifer&display=swap"
             rel="stylesheet"
           />
           <link rel="icon" type="image/x-icon" href="/favicon.ico" />
